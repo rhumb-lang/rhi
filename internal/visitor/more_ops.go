d74936a7250ff6525e17a40e4658d69f12a7c972
MadcapJake <madcap.russo@gmail.com>
2025-12-11 18:38:19 -0600

    Move destructuring op into separate visitor due to grammar change

--- internal/visitor/more_ops.go
+++ internal/visitor/more_ops.go
@@ -14,10 +14,14 @@ func (b *ASTBuilder) VisitApplicative(ctx *grammar.ApplicativeContext) interface
 
 	var op ast.OpType
 	switch opCtx.(type) {
-	case *grammar.FunctionContext: op = ast.OpMakeFn
-	case *grammar.MethodContext: op = ast.OpBindFn
-	case *grammar.LetFunctionContext: op = ast.OpLetFn
-	default: op = ast.OpMakeFn
+	case *grammar.FunctionContext:
+		op = ast.OpMakeFn
+	case *grammar.MethodContext:
+		op = ast.OpBindFn
+	case *grammar.LetFunctionContext:
+		op = ast.OpLetFn
+	default:
+		op = ast.OpMakeFn
 	}
 
 	return &ast.BinaryExpression{Left: left, Op: op, Right: right}
@@ -30,17 +34,30 @@ func (b *ASTBuilder) VisitConditional(ctx *grammar.ConditionalContext) interface
 
 	var op ast.OpType
 	switch opCtx.(type) {
-	case *grammar.PipeContext: op = ast.OpPipe
-	case *grammar.DefaultContext: op = ast.OpCoalesce
-	case *grammar.ForeachContext: op = ast.OpForeach
-	case *grammar.WhileContext: op = ast.OpWhile
-	case *grammar.ThenContext: op = ast.OpIfTrue
-	case *grammar.ElseContext: op = ast.OpIfFalse
-	case *grammar.DestructureContext: op = ast.OpDestruct
-	default: op = ast.OpIfTrue
+	case *grammar.PipeContext:
+		op = ast.OpPipe
+	case *grammar.DefaultContext:
+		op = ast.OpCoalesce
+	case *grammar.ForeachContext:
+		op = ast.OpForeach
+	case *grammar.WhileContext:
+		op = ast.OpWhile
+	case *grammar.ThenContext:
+		op = ast.OpIfTrue
+	case *grammar.ElseContext:
+		op = ast.OpIfFalse
+	default:
+		op = ast.OpIfTrue
 	}
 
 	return &ast.BinaryExpression{Left: left, Op: op, Right: right}
+}
+
+func (b *ASTBuilder) VisitDestructive(ctx *grammar.DestructiveContext) interface{} {
+	left := toExpr(b.Visit(ctx.Expression(0)))
+	right := toExpr(b.Visit(ctx.Expression(1)))
+
+	return &ast.BinaryExpression{Left: left, Op: ast.OpDestruct, Right: right}
 }
 
 func (b *ASTBuilder) VisitPower(ctx *grammar.PowerContext) interface{} {
@@ -50,11 +67,16 @@ func (b *ASTBuilder) VisitPower(ctx *grammar.PowerContext) interface{} {
 
 	var op ast.OpType
 	switch opCtx.(type) {
-	case *grammar.ExponentContext: op = ast.OpPow
-	case *grammar.RootExtractionContext: op = ast.OpRoot
-	case *grammar.RangeContext: op = ast.OpRange
-	case *grammar.ScientificContext: op = ast.OpSciNot
-	default: op = ast.OpPow
+	case *grammar.ExponentContext:
+		op = ast.OpPow
+	case *grammar.RootExtractionContext:
+		op = ast.OpRoot
+	case *grammar.RangeContext:
+		op = ast.OpRange
+	case *grammar.ScientificContext:
+		op = ast.OpSciNot
+	default:
+		op = ast.OpPow
 	}
 
 	return &ast.BinaryExpression{Left: left, Op: op, Right: right}
@@ -79,30 +101,42 @@ func (b *ASTBuilder) VisitDisjunctive(ctx *grammar.DisjunctiveContext) interface
 func (b *ASTBuilder) VisitPrefix(ctx *grammar.PrefixContext) interface{} {
 	expr := toExpr(b.Visit(ctx.Expression()))
 	opCtx := ctx.PrefixOp()
-	
+
 	var op ast.OpType
 	switch opCtx.(type) {
-	case *grammar.EmptyPrefixContext: op = ast.OpIsEmpty
-	case *grammar.FreezePrefixContext: op = ast.OpFreeze
-	case *grammar.CopyPrefixContext: op = ast.OpCopy
-	case *grammar.ToNumberPrefixContext: op = ast.OpToNum
-	case *grammar.NegateNumberPrefixContext: op = ast.OpNegNum
-	case *grammar.ToTruthPrefixContext: op = ast.OpToBool
-	case *grammar.NegateTruthPrefixContext: op = ast.OpNegBool
-	case *grammar.VariadicPrefixContext: op = ast.OpSpread
-	case *grammar.ArgumentPrefixContext: op = ast.OpGetParams
-	case *grammar.SignalOutwardPrefixContext: op = ast.OpSignal
-	case *grammar.SignalInwardPrefixContext: op = ast.OpReply
-	default: op = ast.OpEq
+	case *grammar.EmptyPrefixContext:
+		op = ast.OpIsEmpty
+	case *grammar.FreezePrefixContext:
+		op = ast.OpFreeze
+	case *grammar.CopyPrefixContext:
+		op = ast.OpCopy
+	case *grammar.ToNumberPrefixContext:
+		op = ast.OpToNum
+	case *grammar.NegateNumberPrefixContext:
+		op = ast.OpNegNum
+	case *grammar.ToTruthPrefixContext:
+		op = ast.OpToBool
+	case *grammar.NegateTruthPrefixContext:
+		op = ast.OpNegBool
+	case *grammar.VariadicPrefixContext:
+		op = ast.OpSpread
+	case *grammar.ArgumentPrefixContext:
+		op = ast.OpGetParams
+	case *grammar.SignalOutwardPrefixContext:
+		op = ast.OpSignal
+	case *grammar.SignalInwardPrefixContext:
+		op = ast.OpReply
+	default:
+		op = ast.OpEq
 	}
-	
+
 	return &ast.UnaryExpression{Op: op, Expr: expr}
 }
 
 func (b *ASTBuilder) VisitEffect(ctx *grammar.EffectContext) interface{} {
 	// expression OpenCurly ... patterns ... CloseCurly
 	target := toExpr(b.Visit(ctx.Expression()))
-	
+
 	sel := &ast.SelectorExpression{Patterns: []ast.Pattern{}}
 	if ctx.Patterns() != nil {
 		res := b.Visit(ctx.Patterns())
@@ -110,7 +144,7 @@ func (b *ASTBuilder) VisitEffect(ctx *grammar.EffectContext) interface{} {
 			sel.Patterns = pats
 		}
 	}
-	
+
 	return &ast.EffectExpression{
 		Target:   target,
 		Selector: sel,
