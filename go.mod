b4c773634cf837e4445e94eafb2227cfdad93793
MadcapJake <madcap.russo@gmail.com>
2025-12-11 18:43:29 -0600

    add new allow unsafe version wildcards option and yaml parser

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require github.com/antlr4-go/antlr/v4 v4.13.1
 require (
 	github.com/cockroachdb/apd/v3 v3.2.1 // indirect
 	golang.org/x/exp v0.0.0-20251125195548-87e1e737ad39 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,18 +2,20 @@ package config
 
 // Config holds the configuration for the Rhumb runtime/compiler.
 type Config struct {
-	TraceParser   bool
-	TraceBytecode bool
-	TraceStack    bool
-	TraceSpace    bool
+	TraceParser          bool
+	TraceBytecode        bool
+	TraceStack           bool
+	TraceSpace           bool
+	AllowUnsafeWildcards bool // If false, imports like {-|lib|-} are rejected
 }
 
 // DefaultConfig returns the default configuration.
 func DefaultConfig() *Config {
 	return &Config{
-		TraceParser:   false,
-		TraceBytecode: false,
-		TraceStack:    false,
-		TraceSpace:    false,
+		TraceParser:          false,
+		TraceBytecode:        false,
+		TraceStack:           false,
+		TraceSpace:           false,
+		AllowUnsafeWildcards: false,
 	}
 }
