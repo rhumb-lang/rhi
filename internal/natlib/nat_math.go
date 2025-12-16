9987f182322340e5398e4bc29a2e7ed9ccef72fa
MadcapJake <madcap.russo@gmail.com>
2025-12-16 06:34:04 -0600

    change if to switch

--- internal/natlib/nat_math.go
+++ internal/natlib/nat_math.go
@@ -19,32 +19,33 @@ func natSin(args []mapval.Value) mapval.Value {
 	if len(args) != 1 {
 		return ErrInvalidArg("sin", "expects 1 argument")
 	}
-    
-    // Type Coercion logic (Float/Int -> Float)
-    val := args[0]
-    f := 0.0
-    if val.Type == mapval.ValFloat {
-        f = val.Float
-    } else if val.Type == mapval.ValInteger {
-        f = float64(val.Integer)
-    } else {
-        return ErrInvalidArg("sin", "expects number")
-    }
+
+	// Type Coercion logic (Float/Int -> Float)
+	val := args[0]
+	f := 0.0
+	switch val.Type {
+	case mapval.ValFloat:
+		f = val.Float
+	case mapval.ValInteger:
+		f = float64(val.Integer)
+	default:
+		return ErrInvalidArg("sin", "expects number")
+	}
 
 	return mapval.NewFloat(math.Sin(f))
 }
 
 func natInt(args []mapval.Value) mapval.Value {
-    if len(args) != 2 {
-        return ErrInvalidArg("int", "expects (min, max)")
-    }
-    
-    min, ok1 := args[0].Integer, args[0].Type == mapval.ValInteger
-    max, ok2 := args[1].Integer, args[1].Type == mapval.ValInteger
-    
-    if !ok1 || !ok2 {
-         return ErrInvalidArg("int", "expects integers")
-    }
+	if len(args) != 2 {
+		return ErrInvalidArg("int", "expects (min, max)")
+	}
 
-    return mapval.NewInt(rand.Int63n(max-min) + min)
+	min, ok1 := args[0].Integer, args[0].Type == mapval.ValInteger
+	max, ok2 := args[1].Integer, args[1].Type == mapval.ValInteger
+
+	if !ok1 || !ok2 {
+		return ErrInvalidArg("int", "expects integers")
+	}
+
+	return mapval.NewInt(rand.Int63n(max-min) + min)
 }
