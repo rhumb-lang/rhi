33fdfeb61d2c65cd1879f2b677fe51d3a5d280e7
MadcapJake <madcap.russo@gmail.com>
2025-12-17 11:11:43 -0600

    Add better repl through go-readline-ny and go-multiline-ny

--- cmd/rhi/run.go
+++ cmd/rhi/run.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-	"bufio"
+	"context"
 	"fmt"
+	"io"
 	"os"
+	"regexp"
 	"strings"
+
+	colorable "github.com/mattn/go-colorable"
+
+	readline "github.com/nyaosorg/go-readline-ny"
+	"github.com/nyaosorg/go-readline-ny/simplehistory"
+
+	multiline "github.com/hymkor/go-multiline-ny"
 
 	"github.com/rhumb-lang/rhi/internal/config"
 )
@@ -19,17 +28,62 @@ func (s *Session) runFile(filename string) {
 }
 
 func (s *Session) runREPL() {
-	scanner := bufio.NewScanner(os.Stdin)
+	ctx := context.Background()
 	fmt.Printf("Rhumb REPL (v%s)\n", config.RuntimeVersion.Canonical())
+	fmt.Println("C-m or Enter      : Insert a linefeed")
+	fmt.Println("C-p or UP         : Move to the previous line.")
+	fmt.Println("C-n or DOWN       : Move to the next line")
+	fmt.Println("C-j or Esc+Enter  : Submit")
+	fmt.Println("C-c               : Abort.")
+	fmt.Println("C-D with no chars : Quit.")
+	fmt.Println("C-UP   or M-P     : Move to the previous history entry")
+	fmt.Println("C-DOWN or M-N     : Move to the next history entry")
+	ed, history := newREPL()
 	for {
-		fmt.Print("> ")
-		if !scanner.Scan() {
-			break
+		lines, err := ed.Read(ctx)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			return
 		}
-		line := scanner.Text()
-		if strings.TrimSpace(line) == "" {
-			continue
-		}
-		s.execute(line)
+		joinedLines := strings.Join(lines, "\n")
+		fmt.Println("-----")
+		s.execute(joinedLines)
+		fmt.Println("-----")
+		history.Add(joinedLines)
 	}
+}
+
+func newREPL() (ed multiline.Editor, history *simplehistory.Container) {
+	ed.SetPrompt(func(w io.Writer, lnum int) (int, error) {
+		return fmt.Fprintf(w, "[%d] ", lnum+1)
+	})
+	ed.SetPredictColor(readline.PredictColorBlueItalic)
+
+	ed.Highlight = []readline.Highlight{
+		// Words -> dark green
+		{Pattern: regexp.MustCompile(`(?i)(yes|no)`), Sequence: "\x1B[33;49;22m"},
+		// Double quotation -> light magenta
+		{Pattern: regexp.MustCompile(`(?m)"([^"\n]*\\")*[^"\n]*$|"([^"\n]*\\")*[^"\n]*"`), Sequence: "\x1B[32;49;1m"},
+		// Single quotation -> light red
+		{Pattern: regexp.MustCompile(`(?m)'([^'\n]*\\')*[^'\n]*$|'([^'\n]*\\')*[^'\n]*'`), Sequence: "\x1B[31;49;1m"},
+		// Number literal -> light blue
+		{Pattern: regexp.MustCompile(`[0-9]+`), Sequence: "\x1B[34;49;1m"},
+		// Inline Comment -> dark gray
+		{Pattern: regexp.MustCompile(`(?s)%\(.*?%\)`), Sequence: "\x1B[30;49;1m"},
+		// EOL Comment -> dark gray
+		{Pattern: regexp.MustCompile(`(?s)%[^\(\)=\?].*`), Sequence: "\x1B[30;49;1m"},
+	}
+	ed.ResetColor = "\x1B[0m"
+	ed.DefaultColor = "\x1B[37;49;1m"
+
+	// To enable escape sequence on Windows.
+	// (On other operating systems, it can be ommited)
+	ed.SetWriter(colorable.NewColorableStdout())
+
+	// enable history (optional)
+	history = simplehistory.New()
+	ed.SetHistory(history)
+	ed.SetHistoryCycling(true)
+
+	return
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,19 @@ require (
 	gopkg.in/yaml.v3 v3.0.1
 )
 
-require golang.org/x/exp v0.0.0-20251125195548-87e1e737ad39 // indirect
+require (
+	github.com/clipperhouse/uax29/v2 v2.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/mattn/go-tty v0.0.7 // indirect
+	github.com/nyaosorg/go-box/v3 v3.0.0 // indirect
+	github.com/nyaosorg/go-ttyadapter v0.1.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
+
+require (
+	github.com/hymkor/go-multiline-ny v0.22.3
+	github.com/mattn/go-colorable v0.1.14
+	github.com/nyaosorg/go-readline-ny v1.13.0
+	golang.org/x/exp v0.0.0-20251125195548-87e1e737ad39 // indirect
+)
